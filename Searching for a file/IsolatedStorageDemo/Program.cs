88154c32ef72694cd4ac05fc223675eef1be264e
Farago Bence <semmiertelme13@gmail.com>
2018-09-19 14:13:57 +0200

    Completed Exercise 4 and 5

---
+++ Searching for a file/IsolatedStorageDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.IsolatedStorage;
+
+namespace IsolatedStorageDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IsolatedStorageFile userStore = IsolatedStorageFile.GetUserStoreForAssembly();
+            IsolatedStorageFileStream userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Create, userStore);
+            StreamWriter userWriter = new StreamWriter(userStream);
+            userWriter.WriteLine("User Prefs BLA BLA BLA");
+            userWriter.Close();
+
+            userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Open, userStore);
+            StreamReader userReader = new StreamReader(userStream);
+            string contents = userReader.ReadToEnd();
+            Console.WriteLine(contents);
+            Console.ReadKey();
+
+            }
+    }
+}
