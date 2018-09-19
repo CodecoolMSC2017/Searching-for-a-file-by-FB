6599a5462526809dd15a04670d24cbb9f58ea4dd
Farago Bence <semmiertelme13@gmail.com>
2018-09-19 13:54:38 +0200

    Completed Exercise 3 with advanced Exercise

--- Searching for a file/Searching for a file/Program.cs
+++ Searching for a file/Searching for a file/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 
 namespace SeekAndArchive
 {
     class Program
     {
+        private static List<FileInfo> myFiles = new List<FileInfo>();
         private static List<FileSystemWatcher> watchers = new List<FileSystemWatcher>();
+        private static DirectoryInfo archiveDir;
         static void Main(string[] args)
         {
             String filename = args[0];
@@ -16,7 +19,7 @@ namespace SeekAndArchive
             String pattern = Console.ReadLine();
 
 
-            List<FileInfo> myFiles = new List<FileInfo>();
+            
 
             DirectoryInfo di = new DirectoryInfo(startPath);
             if (!di.Exists)
@@ -27,21 +30,13 @@ namespace SeekAndArchive
 
             }
             Search(di, filename, pattern,ref myFiles);
-            foreach(FileInfo file in myFiles)
+            foreach (FileInfo file in myFiles)
             {
-                
+
                 Console.WriteLine(file.FullName);
-                FileSystemWatcher watcher = new FileSystemWatcher(file.DirectoryName, file.Name);
-                watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
-                   | NotifyFilters.FileName | NotifyFilters.DirectoryName;
-
-                watcher.Changed += new FileSystemEventHandler(OnChanged);
-                watcher.Created += new FileSystemEventHandler(OnChanged);
-                watcher.Deleted += new FileSystemEventHandler(OnChanged);
-                watcher.Renamed += new RenamedEventHandler(OnRenamed);
-
-                watcher.EnableRaisingEvents = true;
+                CreateFileWatcher(file);
             }
+            archiveDir = Directory.CreateDirectory("archivedFiles");
             Console.ReadKey();
            
         }
@@ -70,18 +65,60 @@ namespace SeekAndArchive
                 {
                     
                     Search(dirInfo,fileName,pattern,ref myFiles);
+
+                    }
                 }
             }
-        }
 
         private static void OnChanged(object source, FileSystemEventArgs e)
         {
             Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
+            FileSystemWatcher senderWatcher = (FileSystemWatcher)source;
+            int index = watchers.IndexOf(senderWatcher, 0);
+            ArchiveFile(myFiles[index], index);
         }
 
         private static void OnRenamed(object source, RenamedEventArgs e)
         {
             Console.WriteLine("File: {0} renamed to {1}", e.OldFullPath, e.FullPath);
+            FileSystemWatcher senderWatcher = (FileSystemWatcher)source;
+            int index = watchers.IndexOf(senderWatcher, 0);
+            ArchiveFile(myFiles[index], index);
+        }
+
+        private  static void CreateFileWatcher(FileInfo file)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher(file.DirectoryName, file.Name);
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+               | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+            watcher.EnableRaisingEvents = true;
+            watchers.Add(watcher);
+        }
+
+        private static void ArchiveFile( FileInfo fileToArchive, int index)
+        {
+            FileStream input = fileToArchive.OpenRead();
+            FileStream output = File.Create(archiveDir.FullName + @"\" + fileToArchive.Name + index + ".gz");
+            GZipStream Compressor = new GZipStream(output, CompressionMode.Compress);
+            int b = input.ReadByte();
+            while (b != -1)
+            {
+
+                Compressor.WriteByte((byte)b);
+
+                b = input.ReadByte();
+
+            }
+            Compressor.Close();
+            input.Close();
+            output.Close();
+        }
+
         }
     }
-}
